25c55b204f8468909bc887892c2ae4ee4e351a13
AlexRefery <vip.alx@bk.ru>
2019-07-13 12:42:14 +0300

    More calls

---
+++ src/main/java/io/khasang/gahelp/model/Cat.java
@@ -0,0 +1,83 @@
+package io.khasang.gahelp.model;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+public class Cat implements DisposableBean, InitializingBean, BeanClassLoaderAware, BeanFactoryAware, BeanNameAware, ApplicationContextAware, BeanPostProcessor {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public Cat() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("calling afterPropertiesSet()");
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("Calling @PostConstruct");
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        System.out.println("Calling setBeanClassLoader()");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Calling setBeanFactory()");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Calling setBeanName()");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Calling setApplicationContext()");
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Calling postProcessorBeforeInitialization()");
+        return null;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Calling postProcessorAfterInitialization()");
+        return null;
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        System.out.println("Calling preDestroy");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Calling destroy() DisposableBean");
+    }
+}
